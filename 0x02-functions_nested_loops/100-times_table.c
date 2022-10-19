f45737651d934d853ccba3ec7606dbf4ff6c1fa9
Jemurgor <jemurgorperis01@gmail.com>
2022-10-19 15:54:54 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,16 +15,14 @@ void print_times_table(int n)
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			for (b = 1; b <= n b++)
+			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
 				_putchar(44);
 				_putchar(32);
 				if (op <= 9)
 				{
-					_putchar(32);
-
-					-putchar(32);
+					_putchar(32);				
 					_putchar(op + 48);
 				}
 				else if (op <= 99)
@@ -40,7 +38,7 @@ void print_times_table(int n)
 					_putchar((op % 10) + 48);
 				}
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
