3de95516b59765e2c02b6f463e25d8844f916303
Jemurgor <jemurgorperis01@gmail.com>
2022-10-19 14:27:28 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,22 @@
 
 /**
  * jack_bauer - prints the minute of the day
+ * Return: 0 if successful
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+	int i;
+	int j;
 
-	for ([hour = 0 hour = 23  hour++])
+	for (i = 0; i <= 23; i++)
 	{
-		for ([minute = 0 minute = 59 minute++])
+		for (j = 0; j <= 59; j++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
 		}
 	}
