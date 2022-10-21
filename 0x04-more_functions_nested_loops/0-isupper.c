2ada7ada1767e1c4ac02ab689a5907475cf2c01c
Jemurgor <jemurgorperis01@gmail.com>
2022-10-21 04:35:53 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,15 @@
 /**
  * _isupper - checks for uppercase character
  * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: Always 0
  */
-int _isupper(int c)
+int _isupper(int c);
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{ return (0);
+	}
 }
