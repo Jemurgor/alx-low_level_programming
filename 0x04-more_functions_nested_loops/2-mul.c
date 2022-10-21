aeb922e02d49d03ff65e6b98ea686752e6180fac
Jemurgor <jemurgorperis01@gmail.com>
2022-10-21 04:48:40 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  mul - multiplier
+ *
+ *   @b: int to mult
+ *
+ *   @a: int to mult
+ *
+ *   Return: result of two
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
